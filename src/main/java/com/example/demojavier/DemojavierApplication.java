00d70467da60dac426d59cd4958e5aed1b978f62
Javier Baltar <37340777+JavierBaltar@users.noreply.github.com>
2021-01-27 00:01:47 +0100

    Create DemojavierApplication.java

---
+++ src/main/java/com/example/demojavier/DemojavierApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojavier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojavierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemojavierApplication.class, args);
+	}
+
+}
