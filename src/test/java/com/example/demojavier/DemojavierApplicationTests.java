5feb4a572dae2b606d86e8db76f53277d155141d
Javier Baltar <37340777+JavierBaltar@users.noreply.github.com>
2021-01-27 00:06:35 +0100

    Create DemojavierApplicationTests.java

---
+++ src/test/java/com/example/demojavier/DemojavierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojavier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemojavierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
